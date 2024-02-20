aa4b9de73e40224f8d3eddcfabd60f5d0ef01671
Matt <mweber42@gmail.com>
2024-02-20 10:48:57 -0600

    Add name param

--- slog/apps/api.py
+++ slog/apps/api.py
@@ -3,7 +3,7 @@ from ninja import NinjaAPI
 api = NinjaAPI()
 
 @api.get("/hello")
-def hello(request):
+def hello(request, name):
     test = "Test string"
     test2 = "Restart?"
     return "Hello world"
